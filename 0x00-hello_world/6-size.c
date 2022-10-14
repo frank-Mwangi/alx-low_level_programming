e15b5c8475b27ea56f674ac7734212985ab23572
frank-Mwangi <kungofrank@gmail.com>
2022-10-14 10:52:50 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /*
 * main- Entry point
 * Return: always 0(success)
-* void: parameter
+* @void: parameter
 */
 int main(void)
 {
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n",sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
