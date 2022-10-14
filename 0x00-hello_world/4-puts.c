c8bfd1aa4e0772e4519d748cd5c026634f0d94cd
frank-Mwangi <kungofrank@gmail.com>
2022-10-14 09:33:32 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * Main - Entry 
- *
- * RETURN: Always 0 (Success)
+ *Main - Entry point
+ *RETURN: Always 0 (Success)
  */
 int main(void)
-  {
-	  puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
-  }
+ {
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+ }
