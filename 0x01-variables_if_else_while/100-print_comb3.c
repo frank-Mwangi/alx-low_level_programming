5f407fb3b37a83258535b1dc9c0f1d923a385259
frank-Mwangi <kungofrank@gmail.com>
2022-10-16 12:43:30 -0700

    Two digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of two digits
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = '0'; tens < '9'; tens++)
+	{
+		for (ones = tens + 1; ones <= '9'; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+		if (tens < 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
