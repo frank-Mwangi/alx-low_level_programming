a5d94806b61937a762e6fce288228b1ecb236412
frank-Mwangi <kungofrank@gmail.com>
2022-10-16 08:28:36 -0700

    Print Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * prints the alphabet in lowercase and uppercase,
+ * followed by a newline
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char alph;
+	char Alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph = alph + 1;
+	}
+	Alph = 'A';
+	while (Alph <= 'Z')
+	{
+		putchar (alph);
+	}
+	putchar ('\n')
+		return (0);
+}
