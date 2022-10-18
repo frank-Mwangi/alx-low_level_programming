d6dc9de71d7fccca87b347e0fdfb747ee5d6af7e
frank-Mwangi <kungofrank@gmail.com>
2022-10-18 14:26:35 -0700

    Times 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@ void times_table(void)
 {
 	int r, c;
 
-	for (r = 0; r <= 9; r++)
+	for (r = '0'; r <= '9'; r++)
 	{
-		for (c = 0; c <= 9; c++)
+		for (c = '0'; c <= '9'; c++)
 		{
 			_putchar(r * c);
-			if (c < 9)
+			if (c < '9')
 			{
 				_putchar(',');
 				_putchar(' ');
