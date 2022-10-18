259290df84826aab8bdbb61917a9b799ef824ace
frank-Mwangi <kungofrank@gmail.com>
2022-10-18 13:55:00 -0700

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,24 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = '00'; h < '24'; h++)
+	for (h = 0; h < 24; h++)
 	{
-		_putchar(h);
-		for (m = '00'; m < '60'; m++)
+		for (m = 00; m < 60; m++)
 		{
-		_putchar(m);
-		_putchar('\n');
+		int h1 = (h / 10) + 48;
+		int h2 = (h % 10) + 48;
+		int m1 = (m / 10) + 48;
+		int m2 = (m % 10) + 48;
+
+		_putchar (h1);
+		_putchar (h2);
+		_putchar (':');
+		_putchar (m1);
+		_putchar (m2);
+		putchar ('\n');
+		}
+	}
+}
 		}
 	}
 }
