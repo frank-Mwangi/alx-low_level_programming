40fc9fed85a41a37dbc989921fdf5f288c9b3a9f
frank-Mwangi <kungofrank@gmail.com>
2022-10-21 11:12:57 -0700

    Largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
+#include <stdio.h>
 int positive-or_negative(int);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
