5ab83583590a667f43fbb933b8d05b501f1fdbc4
frank-Mwangi <kungofrank@gmail.com>
2022-10-21 06:16:07 -0700

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,14 +9,8 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-			_putchar('\n');
-		}
-	}
-	else
+	for (i = 1; i <= n; i++)
+		_putchar('_');
 		_putchar('\n');
 }
+
