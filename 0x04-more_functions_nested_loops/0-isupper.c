b86bd44e3265039c2f16e56b23356879f7cf48a4
frank-Mwangi <kungofrank@gmail.com>
2022-10-24 02:07:04 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,20 +10,12 @@
 
 int _isupper(int c)
 {
+	int i;
+
 	if (c >= 65 && c <= 90)
-		return (1);
+		i = 1;
 	else
-		return (0);
+		i = 0;
+	return (i);
 }
-int main(void)
-{
-	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-
-	return (0);
-}
