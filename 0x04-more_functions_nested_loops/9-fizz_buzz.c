282198a37957f1d365484d1783de719d9e60ee06
frank-Mwangi <kungofrank@gmail.com>
2022-10-24 06:14:14 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,26 +13,16 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
+		if (n % 15 == 0)
 			printf("FizzBuzz");
-			printf(" ");
-		}
 		else if (n % 5 == 0)
-		{
 			printf("Buzz");
-			printf(" ");
-		}
 		else if (n % 3 == 0)
-		{
 			printf("Fizz");
-			printf(" ");
-		}
 		else
-		{
 			printf("%d", n);
+		if (n < 100)
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
