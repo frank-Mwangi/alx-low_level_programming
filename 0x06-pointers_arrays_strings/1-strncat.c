ad2f2e5e84635138b45a398e74ad07c38b5c213a
frank-Mwangi <kungofrank@gmail.com>
2022-10-28 10:05:25 -0700

    Concatenate two strings, stop s2 at limit n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: string 1
+ * @src: string 2
+ * @n: integer limit of src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n; j++)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+	}
+	return (dest);
+}
+
