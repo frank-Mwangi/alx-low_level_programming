2920afeb98978ceac30a865db682c1851429d8f6
frank-Mwangi <kungofrank@gmail.com>
2022-10-28 13:55:48 -0700

    Capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes every first letter of words
+ * @str: the input string
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int separator;
+	int i, j;
+
+	separator[13] = {'\n', ',', '\t', ' ', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (*(str + i) - 1 == separator[j] && str[i] >= 97 && str[i] <= 122)
+				str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
+
