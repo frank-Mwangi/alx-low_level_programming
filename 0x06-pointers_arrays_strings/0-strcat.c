142677f5febd8cf98dcd537efb31c9e90b9e5981
frank-Mwangi <kungofrank@gmail.com>
2022-10-28 09:01:45 -0700

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; (dest + i) != '\0'; i++)
+	for (i = 0; *(dest + i) != '\0'; i++)
 	{
 	}
 	j = 0;
-	while ((src + j) != '\0')
+	while (*(src + j) != '\0')
 	{
 		dest[i] = src[j];
 		j++;
