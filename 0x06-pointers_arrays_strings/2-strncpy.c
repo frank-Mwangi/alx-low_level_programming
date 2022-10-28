dcb126076a132900950cd0423760c8ee70d6cc3d
frank-Mwangi <kungofrank@gmail.com>
2022-10-28 10:44:51 -0700

    Copy a string onto another, stop at limit n

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; *(dest + i) != '\0'; i++)
-	{
-	}
-	for (j = 0; j < n && *(src + j) != '\0'; j++)
-	{
+	for (i = 0; i < n && *(dest + i) != '\0'; i++)
 		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
 	return (dest);
 }
+
