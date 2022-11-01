b7fb3855ae302c4263f0bd886f23bd49084749ff
frank-Mwangi <kungofrank@gmail.com>
2022-11-01 11:35:54 -0700

    Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - prints character b in first
+ *		n bytes pointed to by s
+ * @s: pointer
+ * @b: The constant byte to display
+ * @n: memory size
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	s = &b;
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+	}
+	return (s);
+}
