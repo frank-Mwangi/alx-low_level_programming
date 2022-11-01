100b95ad17f252b722c6eb109b2a4826b7d6dfb5
frank-Mwangi <kungofrank@gmail.com>
2022-11-01 11:55:26 -0700

    copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i, j;
 
+	j = 0;
 	for (i = 0; i < n; i++)
-		*(src + i) = *(dest + i);
+	{
+		*(src + i) = *(dest + j);
+		j++;
+	}
 	return (dest);
 }
