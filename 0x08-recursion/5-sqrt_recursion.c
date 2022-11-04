186d5c18b7896a925ce1aa9902dc828f8cc45c03
frank-Mwangi <kungofrank@gmail.com>
2022-11-04 12:52:36 -0700

    Natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,22 +11,21 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return (sqrt(1, n));
+	return (squareroot(1, n));
 }
 
 /**
- * sqrt - guesses the natural square root to
+ * squareroot - guesses the natural square root to
  *	a number
  * @i: square root guesses
  * @j: input integer
  * Return: square root
  */
-int sqrt(int i, int j)
+int squareroot(int i, int j)
 {
 	if (i * i == j)
 		return (i);
 	else if (i * i > j)
 		return (-1);
-	return (sqrt(x + 1, y));
+	return (squareroot(x + 1, y));
 }
-{
