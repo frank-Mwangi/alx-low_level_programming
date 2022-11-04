2e4ece640eddc698c6a180a0abe8b0535da4008b
frank-Mwangi <kungofrank@gmail.com>
2022-11-04 12:46:52 -0700

    Find natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds natural square root
+ *
+ * @n: integer input
+ * Return: squareroot
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqrt(1, n));
+}
+
+/**
+ * sqrt - guesses the natural square root to
+ *	a number
+ * @i: square root guesses
+ * @j: provided integer
+ * Return: square root
+ */
+int sqrt(int i, int j)
+{
+	if (i * i == j)
+		return (i);
+	else if (i * i > j)
+		return (-1);
+	return (sqrt(x + 1, y));
+}
+{
