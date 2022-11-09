72cd4686d96828c9ad65a649df816a404284b4c3
frank-Mwangi <kungofrank@gmail.com>
2022-11-09 12:02:59 -0800

    pointer to a 2D matrix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		c[i] = (int *)malloc(sizeof(int *) * width);
+		c[i] = (int *)malloc(sizeof(int) * width);
 		if (c[i] == NULL)
 		{
 			while (i >= 0)
