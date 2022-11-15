be160a3b5b500543054307bfce0fc71e36741a59
frank-Mwangi <kungofrank@gmail.com>
2022-11-15 10:52:42 -0800

    Function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * new_dog - defines a new dog
  * @name: new dog's name
@@ -40,6 +40,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (k = 0; k <= j; k++)
 		ptr->owner[k] = owner[k];
 	return (ptr);
-}
-	return (new_dog);
 }
