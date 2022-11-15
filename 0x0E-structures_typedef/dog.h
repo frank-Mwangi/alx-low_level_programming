7d1ba212a0775c9a9b93f9b59cafb2491a25d5c1
frank-Mwangi <kungofrank@gmail.com>
2022-11-15 09:05:50 -0800

    defining poppy with struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - defines dog qualities
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ *
+ * Description: Let's find out more about this
+ *		dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* main.h */
