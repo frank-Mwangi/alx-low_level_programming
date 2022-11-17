ebec17569dd9c80c76ceea56556076997664a3bd
frank-Mwangi <kungofrank@gmail.com>
2022-11-17 09:19:43 -0800

    Function that prints a name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+#endif /*FUNCTION_POINTERS_H_*/
