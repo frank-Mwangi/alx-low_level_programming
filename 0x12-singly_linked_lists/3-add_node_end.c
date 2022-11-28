dc55b69c7c4960fcb8d7f8f892723485486bfcb1
frank-Mwangi <kungofrank@gmail.com>
2022-11-28 10:19:32 -0800

    Function that adds node to end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,7 +27,10 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->next = NULL;
 	if (*head == NULL)
+	{
 		*head = new_node;
+		return (*head);
+	}
 	else
 	{
 		nav = *head;
@@ -37,5 +40,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 		nav->next = new_node;
 	}
-	return (new_node);
+	return (*head);
 }
