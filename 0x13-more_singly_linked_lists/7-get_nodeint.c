0aedd5654fd8a55c0fbaebda8e9904a807958bca
frank-Mwangi <kungofrank@gmail.com>
2022-12-01 00:10:25 -0800

    Find nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,23 +3,21 @@
 /**
  * get_nodeint_at_index - finds the nth node in linked list
  * @index: position of node
- *
+ * @head: head pointer
  * Return: pointer to node n
  *
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *nav;
 	unsigned int i = 0;
 
-	nav = head;
-	while (nav != NULL)
+	while (i < index)
 	{
-		nav = nav->next;
+		head = head->next;
+		if (head->next == NULL)
+			return (NULL);
 		i++;
-		if (i == index)
-			return (nav);
 	}
-	return (NULL);
+	return (head);
 }
