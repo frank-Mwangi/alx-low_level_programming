4d5d1f11ce2f410cf15bf25ba491d7f4ec94391d
frank-Mwangi <kungofrank@gmail.com>
2022-12-06 10:46:07 -0800

    Clear bit at given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets bit at given index to 0
+ * @n: integer provided
+ * @index: position of value to be changed
+ * Return: 1 for success, -1 otherwise
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+	*n = *n | (0 << index);
+	return (1);
+}
+
