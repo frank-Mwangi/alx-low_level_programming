221aa84ab9bb5ecb09c6ccecf39aebe6453ca247
frank-Mwangi <kungofrank@gmail.com>
2022-12-07 11:56:55 -0800

    Create an empty file and write into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(op, text_content, strlen(text_content));
 
 	if (op == -1 || wrt == -1)
