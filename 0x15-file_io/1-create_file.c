835174ee279ee1ca0e3da9aa03ea3cdc3ff130a6
frank-Mwangi <kungofrank@gmail.com>
2022-12-07 11:53:11 -0800

    Create a file and write into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int op, rd, wr;
+	int op, wrt;
 
 	if (filename == NULL)
 		return (-1);
 	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	rd = read(op, text_content, strlen(text_content));
-	wr = write(rd, text_content, strlen(text_content));
+	wrt = write(op, text_content, strlen(text_content));
 
+	if (op == -1 || wrt == -1)
+		return (-1);
 	close(op);
 	return (1);
 }
