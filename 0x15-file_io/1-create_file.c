0e255c8cf72d03f40daf7d2a9080f3df5ed90300
frank-Mwangi <kungofrank@gmail.com>
2022-12-07 11:37:47 -0800

    Creates an empty file and writes into it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes in it
+ * @filename: file to be created
+ * @text_content: content to write in file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, op, rd, wr;
+
+	if (filename == NULL)
+		return (-1);
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	rd = read(op, text_content, strlen(text_content));
+	wr = write(rd, text_content, strlen(text_content));
+
+	close(op);
+	return (1);
+}
