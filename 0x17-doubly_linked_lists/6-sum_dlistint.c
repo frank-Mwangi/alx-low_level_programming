30d5157ff5f9495f6580c5dec0a24466dbc97b8e
frank-Mwangi <kungofrank@gmail.com>
2023-01-12 12:08:13 -0800

    Sum all node values in DLL

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,13 +11,10 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	if (head == NULL)
-		return (0);
-	while (head->next != NULL)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-	sum += head->n;
 	return (sum);
 }
