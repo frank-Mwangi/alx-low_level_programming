89758c5750133184458455bc8eddff5af4735f47
frank-Mwangi <kungofrank@gmail.com>
2023-01-12 12:48:08 -0800

    Insert node at given index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,16 +11,27 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp, *new;
-	unsigned int i;
+	unsigned int i = 0, len = len_node(h);
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	temp = *h;
-	for (i = 0; i < (idx - 1); i++)
+	while (temp)
 	{
+		if (i == idx - 1)
+			break;
+		else if (i < idx -1 && temp == NULL)
+			return (NULL);
 		temp = temp->next;
+		i++;
 	}
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+	if (len == idx)
+		return (add_dnodeint_end(h, n));
+	else if (len < idx)
+		return (NULL);
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
@@ -28,4 +39,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next->prev = new;
 
 	return (new);
+}
+
+/**
+ * len_node - finds length of DLL
+ *
+ * @node: pointer to DLL
+ * Return: size of DLL
+ */
+unsigned int len_node(dlistint_t **node)
+{
+	unsigned int len = 0;
+	dlistint_t *temp;
+
+	temp = *node;
+	while (temp != NULL)
+	{
+		len += 1;
+		temp = temp->next;
+	}
+	return (len);
 }
